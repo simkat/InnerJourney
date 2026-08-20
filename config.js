5b376ed45911bac5cfa377196b69db8e3e50ee72
simkat <simkatyal1@gmail.com>
2026-08-20 02:37:06 +0000

    chore: externalize app config

---
+++ config.js
@@ -0,0 +1,6 @@
+/* Inner Journey deployment configuration.
+   Keep this file when regenerating index.html. */
+const CONFIG = {
+  ENDPOINT: "https://script.google.com/macros/s/AKfycbwa1BdQ0L9wKmSZTyX6g1tCOz-ljCm1CkhQTZSk1eBcB6exgi1JJIT_iKpVnmn-N-eKmQ/exec",
+  SECRET: "pray"
+};
